0eb2a5208ae8644ddfeb1671e5bfdb2b94224625
Sminotti <sebastianminotti@gmail.com>
2024-07-31 22:21:41 -0300

    haciendo el filtro de busqueda

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,9 +1,12 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input, OnInit, viewChild, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { EmpleadosService } from '@services/empleados.service';
 import { Empleado } from '@models/empleados.interface';
-import { MatPaginatorModule } from '@angular/material/paginator';
-const MATERIAL_MODULES = [MatPaginatorModule, MatTableModule];
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+const MATERIAL_MODULES = [MatPaginatorModule, MatTableModule,MatSortModule,MatInputModule,MatFormFieldModule];
 
 @Component({
   selector: 'app-Table-component',
@@ -18,8 +21,18 @@ export class TableComponent<T> implements OnInit {
   data = input.required<T[]>();
 
   dataSource = new MatTableDataSource<T>();
+  private readonly _sort = viewChild.required<MatSort>(MatSort); // se declara privada
+private readonly _paginator = viewChild.required<MatPaginator>(MatPaginator);
 
   ngOnInit(): void {
     this.dataSource.data = this.data();
+    this.dataSource.sort = this._sort();
+    this.dataSource.paginator = this._paginator();
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  
 }
